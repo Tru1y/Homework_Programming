c18fa67ea68bd5eb5a3043ece7562a120da847ef
Tru1y <the.reaalshd@gmail.com>
2025-02-22 14:04:00 -0500

    hw

--- Assets/Scripts/PlayerDie.cs
+++ Assets/Scripts/PlayerDie.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class PlayerDie : MonoBehaviour
 {
+    private string Enemy = ("Enemy");
+    public GameObject endPannel;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Enemy ) ;
+        {
+            endPannel.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
 }
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,5 +2,26 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public GameObject preFab2;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject Bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+
+            Bullet.transform.SetParent(bulletTrash);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject Bullet = Instantiate(preFab2, bulletSpawn.position, Quaternion.identity);
+
+            Bullet.transform.SetParent(bulletTrash);
+        }
+    }
 }
